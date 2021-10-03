dd7b46a412ade6f0922882df7363ff1525b897ef
rayquen <rayquen.zero@gmail.com>
2021-10-03 00:07:57 -0300

    lectura de ultima cotizacion

--- worker.js
+++ worker.js
@@ -55,3 +55,51 @@ chrome.runtime.onInstalled.addListener(() => {
 
     saveConfig()
 })
+
+//update money
+setInterval(updateMoney, 10 * 60 * 1000)
+
+
+function updateMoney() {
+
+    fetch(`https://api.coingecko.com/api/v3/coins/plant-vs-undead-token`)
+        .then(response => response.json())
+        .then(data => {
+            console.log('[updateMoney] start')
+
+            const le2pvu = 550
+            const pvu2le = 105
+
+            const pvu2usd = data.market_data.current_price.usd
+            const pvu2ars = data.market_data.current_price.ars * 2
+
+            config.money.LE.quote.PVU = 1/le2pvu   //Swap price LE to PVU
+            config.money.PVU.quote.LE = pvu2le //Swap price PVU to LE
+
+            config.money.PVU.quote.USD = pvu2usd
+            config.money.PVU.quote.ARS = pvu2ars
+            
+            config.money.USD.quote.ARS = pvu2ars/pvu2usd
+            config.money.USD.quote.PVU = 1/pvu2usd
+            config.money.USD.quote.LE = pvu2le / pvu2usd
+
+            config.money.ARS.quote.USD = 1/config.money.USD.quote.ARS
+            config.money.ARS.quote.PVU = 1/pvu2ars
+            config.money.ARS.quote.LE = pvu2le/pvu2ars
+
+            config.money.LE.quote.USD = pvu2usd / le2pvu
+            config.money.LE.quote.ARS = pvu2ars / le2pvu
+
+            console.log('[updateMoney] config')
+            console.log(config.money)
+
+            saveConfig()
+        }).catch(e => {
+            console.error('[updateMoney]',e)
+        })
+
+}
+
+function saveConfig(){
+    chrome.storage.sync.set({ config })
+}
